d3b47a040f4666d6316b03152feb6e945de30f17
Kristina <kamagorovakristina@gmail.com>
2025-08-15 00:34:23 +0700

    Edit CSS App, create arr todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import todos from "./todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <div className="container">
+      <nav className="navbar is-light">
+        <div className="navbar-brand">
+          <span className="navbar-item is-uppercase">
+            ToDos-1
+          </span>
+        </div>
+      </nav>
+      <main className="content px-6 py-6">
+        <h1>Todos</h1>
+      </main>
+    </div> 
+    );
 }
-
-export default App;
