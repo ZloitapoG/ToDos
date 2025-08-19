5db27758f4afeab8370917f859d098eab7431620
Kristina <kamagorovakristina@gmail.com>
2025-08-19 16:13:00 +0700

    Routes / , /add; Outlet, NavLink

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import TodoList from "./TodoList";
+//import TodoList from "./TodoList";
 import { useState } from "react";
-import TodoAdd from "./TodoAdd";
+//import TodoAdd from "./TodoAdd";
 import initialTodos from './todos';
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function App() {
   const [todos, setTodos] = useState(initialTodos)
@@ -23,15 +24,37 @@ export default function App() {
     <div className="container">
       <nav className="navbar is-light">
         <div className="navbar-brand">
-          <span className="navbar-item is-uppercase">
+          {/*<span className="navbar-item is-uppercase">
             ToDos-1
-          </span>
+          </span>*/}
+          <NavLink 
+              to="/"
+              className={({isActive})=> 
+                'navbar-item is-uppercase' + 
+                (isActive ? ' is-active': '')
+              }>
+                Делишки
+          </NavLink>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+              <NavLink
+                to = "/add"
+                className = {({isActive})=>
+                  'navbar-item' + 
+                  (isActive ? ' is-active' : '')
+                }
+                >
+                  Создать дельце
+              </NavLink>
+          </div>
         </div>
       </nav>
       <main className="content px-6 py-6">
         {/*<h1>Todos</h1>*/}
-        <TodoList list={todos} setDone={setDone} del={del} />
-        <TodoAdd add = {add}/>
+        {/*<TodoList list={todos} setDone={setDone} del={del} />
+        <TodoAdd add = {add}/>*/}
+        <Outlet/>
       </main>
     </div>
   );
